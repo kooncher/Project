899920559bf05f52e39c367cbf2a505bda798ff1
kooncher <75032953+kooncher@users.noreply.github.com>
2020-12-20 19:27:47 +0700

    Add files via upload

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Login</h2>
+        <div class="row">
+        <div class="card mx-auto">
+        <div class="card-header text-white bg-primary">
+            <h4>Login</h4>
+            </div>
+    
+    </div>
+    </div>
+    </div>
+
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
